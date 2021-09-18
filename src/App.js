05e816063b3facb64a9591ff046442315288a74c
rasel <russelroy15@gmail.com>
2021-09-18 20:57:38 +0600

    WIP core React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <LoadComments></LoadComments>
+    </div>
+  );
+}
+
+function LoadComments() {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setComments(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Load Comment</h3>
+      {comments.map((comment) => (
+        <Comment title={comment.title} body={comment.body}></Comment>
+      ))}
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div>
+      <h4>{props.title}</h4>
+      <p>{props.body}</p>
     </div>
   );
 }
