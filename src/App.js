29c9bba381c54619f8f06f460f341ecad321e929
rasel <russelroy15@gmail.com>
2021-09-18 20:47:30 +0600

    WIP core React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increaseNumber = () => setCount(count + 1);
+  const decreaseNumber = () => setCount(count - 1);
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={increaseNumber}>INCREASE</button>
+      <button onClick={decreaseNumber}>DECREASE</button>
+    </div>
+  );
 }
 
 export default App;
